1b62174b71f32f83e3855815b3cfc9b8a6af9ba5
batra2304 <37112041+batra2304@users.noreply.github.com>
2018-07-02 15:32:03 +0530

    pata ni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,9 @@ const appRoutes: Routes = [
   { path: 'footer', component: FooterComponent },
   { path: 'login', component: LoginComponent },  
   { path: 'signup', component: SignupComponent },
-  { path: 'mehandi', component: MehandiComponent }, 
+  { path: 'mehandi', component: MehandiComponent },
+  { path: 'caterers', component:  CaterersComponent } 
+
 
 ];
 
