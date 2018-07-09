62cbf7d18779f87a2a48f8799fbefc06a555a4fd
batra2304 <37112041+batra2304@users.noreply.github.com>
2018-07-09 22:03:54 +0530

    sir ne jo change kara

--- src/app/dj/dj.component.ts
+++ src/app/dj/dj.component.ts
@@ -11,27 +11,66 @@ export class DjComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  catList = [
+    {
+      catCode:'arabic',
+      cat: 'Arabic',
+      checked:true
+    },
+    {
+      catCode:'indian',
+      cat: 'Indian',
+      checked:false
+    },
+    {
+      catCode:'pakistan',
+      cat: 'Pakistan',
+      checked:false
+    }
+  ];
+  catChecked(index){
+    // console.log("hi");1
+    this.catList[index].checked = !this.catList[index].checked;
+    console.log(this.catList);  
+  }
 
-  meh() {
+  // myFunction() {
+  //   // Get the checkbox
+  //   var checkBox = document.getElementById("meh");
+  //   // Get the output text
+  //   var text = document.getElementById("text");
+  
+  //   // If the checkbox is checked, display the output text
+  //   if (checkBox.checked == true){
+  //     text.style.display = "block";
+  //   } else {
+  //     text.style.display = "none";
+  //   }
+  //   console.log("JIII00");
+  // }}
+
+//   meh() {
    
-    var checkBox = document.getElementById("meh");
-    var text = document.getElementById("text").style.display="block";
-    var text = document.getElementById("text1").style.display="none";
+//     var checkBox = document.getElementById("meh");
+//     var text = document.getElementById("text").style.display="block";
+//     var text = document.getElementById("text1").style.display="none";
     
     
     
-    // if (checkBox.check == true){
-    //   text.style.display = "block";
-    // } else {
-    //   text.style.display = "none";
-    // }
+//     // if (checkBox.check == true){
+//     //   text.style.display = "block";
+//     // } else {
+//     //   text.style.display = "none";
+//     // }
 
   
-  }
-  myFunction(){
+//   }
+//   myFunction(){
     
-    var text = document.getElementById("text1").style.display="block";
-    var text = document.getElementById("text").style.display="none";
-  }
+//     var text = document.getElementById("text1").style.display="block";
+//     var text = document.getElementById("text").style.display="none";
+//   }
 
+// }
 }
